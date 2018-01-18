8d8bb7b22ad47d051fc9ae8fe1941de67647bfb6
BiggerCrayonZ <32489142+BiggerCrayonZ@users.noreply.github.com>
2018-01-18 16:44:55 -0600

    Modificación del Sistema de Actualización de Avatares

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,31 +1,25 @@
 <!--Hereda del template Main -->
-@extends ('admin.template.main') 
- 
+@extends ('admin.template.main')
+
 <!--Contenido a mostrar -->
-@section('content')
-<div class="container">
-  <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <img class="img_profile rounded-circle" width="100px" height="100px" src="{{ asset('uploads/avatars/'.$user->avatar) }}">
-        <!--Imprime el nombre completo del usuario -->
-        <h2>{{ $user->username }}</h2>
-        <h4>Cambiar Avatar</h4>
-        <!--Formulario para actualizar foto de perfil -->
-        {{ Form::open(['route' => ['user.profile.update'], 'files' => true, 'method' => 'PATCH']) }}
-          <div class="form-group">
-            
-            <br>
-            <div class="custom-file">
-              <!--Forma para subir archivos -->
-              {{ Form::file('avatar', ['class' => 'image']) }}
-            </div>
-            <!--Botón Submit-->
-            {{ Form::submit('Subir', ['name' => 'submit', 'class'=>'btn btn-success']) }}
-            <br>
-            <small class="text-muted">Tamaño Max: 2MB - Aceptable: .png, .jpb, .bmp  </small>
-          </div>
-        {{ Form::close() }}
+@section('content') 
+
+  <div class="card">
+      {{ Form::open(['route' => ['user.profile.update'], 'files' => true, 'method' => 'PATCH']) }}
+    <img class="img_profile rounded-circle" width="100px" height="100px" src="{{ asset('uploads/avatars/'.$user->avatar) }}">
+    <div class="card-body">
+      <h4 class="card-title">{{ $user->username }}</h4>
+      <p class="card-text">Cambiar Avatar</p>
+      <div class="custom-file">
+        <!--Forma para subir archivos -->
+        {{ Form::file('avatar', ['class' => 'image']) }}
+      </div>
+
+      <!--Botón Submit-->
+      {{ Form::submit('Subir', ['name' => 'submit', 'class'=>'btn btn-success']) }}
+      <small class="text-muted">Tamaño Max: 2MB - Aceptable: .png, .jpb, .bmp </small>
     </div>
+    {{ Form::close() }} 
   </div>
-</div>
+
 @endsection
